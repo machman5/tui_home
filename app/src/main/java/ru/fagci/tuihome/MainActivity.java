ce2984fd637d0222ee79bde77e0433829690f1a0
Mikhail Yudin <yumih@r-o.ru>
2019-12-03 17:27:35 +0700

    cleanup

--- app/src/main/java/ru/fagci/tuihome/MainActivity.java
+++ app/src/main/java/ru/fagci/tuihome/MainActivity.java
@@ -6,7 +6,6 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
-import androidx.loader.app.LoaderManager;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.neurenor.permissions.PermissionsHelper;
@@ -14,15 +13,11 @@ import ru.fagci.tuihome.model.ModelObject;
 import ru.fagci.tuihome.vm.AppViewModel;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import static android.Manifest.permission.*;
 
 public class MainActivity extends AppCompatActivity  {
-
-    private static HashMap<String, Long> timing = new HashMap<>();
-
     private PermissionsHelper permissionHelper;
     private final String[] permissions = new String[]{
             CALL_PHONE,
@@ -32,7 +27,6 @@ public class MainActivity extends AppCompatActivity  {
             WRITE_EXTERNAL_STORAGE
     };
 
-    private static LoaderManager loaderManager;
     private static List<ModelObject> modelObjects = new ArrayList<>();
 
     private TextView output;
@@ -57,46 +51,6 @@ public class MainActivity extends AppCompatActivity  {
 //        r.run();
 //    }
 
-//    @NonNull
-//    @Override
-//    @SuppressWarnings("unchecked cast")
-//    public Loader<List<ModelObject>> onCreateLoader(int loaderId, @Nullable Bundle args) {
-//        Loader<?> loader = null;
-//
-//        switch (loaderId) {
-//            case LOADER_CONTACTS:
-//                loader = new ContactLoaderTask(getApplicationContext());
-//                break;
-//            case LOADER_APPS:
-//                loader = new AppLoaderTask(getApplicationContext());
-//                break;
-//            case LOADER_MEDIA:
-//                loader = new MediaLoaderTask(getApplicationContext());
-//                break;
-//        }
-//
-//        if (loader != null) {
-//            String name = loader.getClass().getSimpleName();
-//            timing.put(name, System.nanoTime());
-//            output.append(name + " start\n");
-//        }
-//        return (Loader<List<ModelObject>>) loader;
-//    }
-
-//    @Override
-//    public void onLoadFinished(@NonNull Loader<List<ModelObject>> loader, List<ModelObject> items) {
-//        modelObjects.addAll(items);
-//        cmdChainAdapter.edit().replaceAll(modelObjects).commit();
-//        String name = loader.getClass().getSimpleName();
-//        long t = System.nanoTime() - timing.get(name);
-//        output.append(name + " finished (" + items.size() + "), " + String.format(Locale.getDefault(), "%.2fms", t / 1000000.0f) + "\n");
-//    }
-//
-//    @Override
-//    public void onLoaderReset(@NonNull Loader<List<ModelObject>> loader) {
-//        output.append(loader.getClass().getSimpleName() + " reset\n");
-//    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -116,9 +70,7 @@ public class MainActivity extends AppCompatActivity  {
         cmdChain.setAdapter(cmdChainAdapter);
         cmdChain.setNestedScrollingEnabled(false);
 
-//        loaderManager = getSupportLoaderManager();
-//        loaderManager.initLoader(LOADER_APPS, null, this);
-//
+
 //        permissionHelper = new PermissionsHelper(this);
 //        permissionHelper.requestPermissions(permissions, p -> {
 //            for (String perm : p.keySet()) {
@@ -134,7 +86,7 @@ public class MainActivity extends AppCompatActivity  {
 //                }
 //            }
 //        });
-//
+
 //        cmdLine.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
 //            @Override
 //            public boolean onQueryTextSubmit(String p1) {
